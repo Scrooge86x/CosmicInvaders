38d07674e5d4de33be93f337f18156e9f2edd2c1
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-11-05 19:26:27 +0100

    Initialize project structure and add GLFW example

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window{ glfwCreateWindow(900, 600, "Test Window", NULL, NULL) };
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
