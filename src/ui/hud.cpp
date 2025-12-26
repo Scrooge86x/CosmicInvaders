2e0035c37c8699afaa69d8b50421edf7c5638717
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-12-26 16:06:24 +0100

    Implement custom fps display in drawHud

--- src/ui/hud.cpp
+++ src/ui/hud.cpp
@@ -2,10 +2,19 @@
 
 #include <gameplay/game.h>
 #include <imgui.h>
+#include <format>
 
 void ui::drawHud(Game& game) {
-    ImGui::Begin("Hud Placeholder");
-    ImGui::Text("Fps: %lf", game.getFps());
-    ImGui::Text("Press ESCAPE to open the pause menu.");
-    ImGui::End();
+    const ImGuiViewport* mainViewPort{ ImGui::GetMainViewport() };
+    ImDrawList* const drawList{ ImGui::GetForegroundDrawList() };
+
+    const std::string fpsText{ std::format("{:.2f} FPS", game.getFps()) };
+    const ImVec2 textSize{ ImGui::CalcTextSize(fpsText.c_str()) };
+
+    constexpr float padding{ 10.f };
+    drawList->AddText(
+        { mainViewPort->WorkSize.x - textSize.x - padding, padding },
+        IM_COL32_WHITE,
+        fpsText.c_str()
+    );
 }
