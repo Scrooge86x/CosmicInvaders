f9b1083705407cd0e9e561b7763c83c4f934a5c2
MrAlantari <piorko050@gmail.com>
2026-01-15 00:44:39 +0100

    Change how createEntity works.

--- src/ecs/entities.cpp
+++ src/ecs/entities.cpp
@@ -1,5 +1,6 @@
 #include "entities.h"
 #include <renderer/model.h>
+#include <renderer/model-store.h>
 
 entt::entity createPlayer(entt::registry& registry, std::shared_ptr<Model> object, const glm::vec3& position) {
 	entt::entity entity = registry.create();
@@ -14,13 +15,39 @@ entt::entity createPlayer(entt::registry& registry, std::shared_ptr<Model> objec
 	return entity;
 }
 
-entt::entity createEntity(entt::registry& registry, std::shared_ptr<Model> object, const glm::vec3& position) {
+entt::entity createEntity(entt::registry& registry, EnemyType enemyType, ModelStore& modelStore, Lane::Lane& lane) {
 
 	entt::entity entity = registry.create();
+	std::shared_ptr<Model> object;
+	glm::vec3 position{ Lane::getLaneXPosition(lane), -2.0f, -100.0f};
+	glm::vec3 velocity{0.0f, 0.0f, 2.0f};
+	float health{ 5.0f };
+
+	constexpr auto basicEnemy{ "" };
+	constexpr auto slimEnemy{ "" };
+	constexpr auto bulkyEnemy{ "" };
+
+	switch (enemyType) {
+	case EnemyType::Basic:
+		object = modelStore.load(basicEnemy, 0.0003f);
+		break;
+	case EnemyType::Slim:
+		object = modelStore.load(slimEnemy, 0.0003f);
+		velocity.z = 4.0f;
+		health = 3.0f;
+		break;
+	case EnemyType::Bulky:
+		object = modelStore.load(bulkyEnemy, 0.0003f);
+		velocity.z = 0.5f;
+		health = 8.0f;
+		break;
+	default:
+		break;
+	}
 
 	registry.emplace<Transform>(entity, position, glm::vec3{0.f, 90.f, 0.f});
-	registry.emplace<Velocity>(entity);
-	registry.emplace<Health>(entity, 5.0f, 5.0f);
+	registry.emplace<Velocity>(entity, velocity);
+	registry.emplace<Health>(entity, health, health);
 	registry.emplace<Render>(entity, std::move(object));
 	registry.emplace<Damage>(entity, 3.0f);
 	registry.emplace<TimeDelay>(entity);
--- src/ecs/entities.h
+++ src/ecs/entities.h
@@ -5,9 +5,16 @@
 
 #include "components.h"
 class Model;
+class ModelStore;
+
+enum class EnemyType {
+	Basic,
+	Slim,
+	Bulky
+};
 
 entt::entity createPlayer(entt::registry& registry, std::shared_ptr<Model> object, const glm::vec3& position);
-entt::entity createEntity(entt::registry& registry, std::shared_ptr<Model> object, const glm::vec3& position);
+entt::entity createEntity(entt::registry& registry, EnemyType enemyType, ModelStore& modelStore, Lane::Lane& lane);
 entt::entity createBullet(entt::registry& registry, const EntityTypes fromWho, std::shared_ptr<Model> object, glm::vec3& position, const glm::vec3& velocity);
 
 #endif // !ENTITIES_H
