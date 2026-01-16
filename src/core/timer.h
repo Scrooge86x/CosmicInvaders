238916e1df5cd7dfa78621830ac5413a9363dc8c
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2026-01-16 01:17:09 +0100

    Add Doxyfile and document the Timer class

--- src/core/timer.h
+++ src/core/timer.h
@@ -5,10 +5,24 @@
 
 #include <concepts>
 
+/**
+ * @brief Measures delta time between updates.
+ */
 class Timer {
 public:
+    /**
+     * @brief Updates the current delta time based on the previous timestamp.
+     *
+     * This function should be called once per frame to measure the time between frames.
+     */
     void update();
 
+    /**
+     * @brief Returns the stored delta time from the last update, does NOT recompute it.
+     *
+     * @tparam T Floating point type to return the value as.
+     * @return Delta time in seconds.
+     */
     template <std::floating_point T>
     [[nodiscard]] T getDt() const {
         return static_cast<T>(m_dt);
